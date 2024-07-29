c8ca37a73105c91daa1ab006657d0425d9e25d57
Lucky2081 <luckyjasmine2081@gmail.com>
2024-07-29 22:05:01 +0800

    server: add flask bp.

--- server/api.py
+++ server/api.py
@@ -11,8 +11,11 @@ from extends import (
 )
 from apps.demo.models import DemoModel
 from apps.demo import bp as demo_bp
-
-
+# from apps.weibo_UI import bp as weibo_bp
+# from apps.RAG_Vector import bp as vector_bp
+# from apps.RAG_Document import bp as document_bp
+from apps.Inference import bp as inference_bp
+from apps.Content_Creator import bp as content_creator_bp
 # TODO: Import new blueprint here
 
 
@@ -20,8 +23,12 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
     app.register_blueprint(demo_bp)
+    # app.register_blueprint(weibo_bp)
+    # app.register_blueprint(vector_bp)
+    # app.register_blueprint(document_bp)
+    app.register_blueprint(inference_bp)
+    app.register_blueprint(content_creator_bp)
     # TODO: Register new blueprint here
-
     db.init_app(app)
 
     return app
---
+++ server/apps/Inference/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Local application/library specific imports.
+from apps.Inference.views import bp
