b7836a100365aa236ba60bed6176d010893b4a66
Lucky2081 <luckyjasmine2081@gmail.com>
2024-07-31 04:11:12 +0800

    server: add render_template demo and dir.

--- server/apps/demo/views.py
+++ server/apps/demo/views.py
@@ -2,13 +2,19 @@
 # Standard library imports.
 import sqlite3
 # Related third party imports.
-from flask import Flask, Blueprint
+from flask import Blueprint, render_template
 from loguru import logger
 
 # Local application/library specific imports.
 
 
 bp = Blueprint("demo", __name__, url_prefix='/demo')
+
+
+@bp.route('/')
+def index():
+    logger.info('run index')
+    return render_template('demo/index.html')
 
 
 @bp.route('/heartbeat')
