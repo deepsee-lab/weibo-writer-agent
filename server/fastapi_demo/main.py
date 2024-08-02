3fea1d212c65dca617fd1c239176140d561a18b2
Lucky2081 <luckyjasmine2081@gmail.com>
2024-08-02 08:11:44 +0800

    server: add fastapi demo.

--- server/fastapi_demo/main.py
+++ server/fastapi_demo/main.py
@@ -1,10 +1,25 @@
 import uvicorn
-from fastapi import FastAPI
 from loguru import logger
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from apps.demo.views import router as demo_router
 
 app = FastAPI(
-    description="api文档: demo"
+    title="FastAPI",
+    description="FastAPI",
+    version="0.0.1",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+# 允许所有来源
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 允许访问的源列表
+    allow_credentials=True,  # 支持 cookies 跨域
+    allow_methods=["*"],  # 允许的请求方法
+    allow_headers=["*"],  # 允许的请求头
 )
 
 app.include_router(demo_router)
