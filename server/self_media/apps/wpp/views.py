b9f81ce527263532c7cac50717154ea5ee3508bd
Lucky2081 <luckyjasmine2081@gmail.com>
2024-08-03 01:59:46 +0800

    server: add self_media.

--- server/self_media/apps/wpp/views.py
+++ server/self_media/apps/wpp/views.py
@@ -1,12 +1,34 @@
 from loguru import logger
 from fastapi import APIRouter
+from pydantic import BaseModel, Field
 
 router = APIRouter(
     prefix="/wpp"
 )
 
 
+class DraftAddItem(BaseModel):
+    title: str
+    content: str
+    thumb_media_id: str
+
+
+class Response(BaseModel):
+    success: bool
+    code: str
+    message: str
+    data: dict = None
+
+
 @router.get("/heartbeat")
 def heartbeat():
     logger.info('run heartbeat')
     return 'heartbeat'
+
+
+@router.post("/draft_add")
+def draft_add(item: DraftAddItem):
+    logger.info('run draft_add')
+    logger.info(item)
+    data = {}
+    return Response(success=True, code='000000', message='success', data=data)
