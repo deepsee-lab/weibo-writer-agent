22774a9be49517f24a82828e4527b92b91f96db2
wwwwwald <yf.wang298@gmail.com>
2024-08-03 11:57:20 +0800

    webui_modify

--- server/web/configs/config.py
+++ server/web/configs/config.py
@@ -31,5 +31,8 @@ cst_tz = pytz.timezone('Asia/Shanghai')
 CUS_DB_PATH = os.path.join(ROOT_DIR, 'db.sqlite3')
 SQLALCHEMY_DATABASE_URI = 'sqlite:////{}'.format(CUS_DB_PATH)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+# upload file format
+MAX_CONTENT_LENGTH = 160 * 1024 * 1024
+UPLOAD_FOLDER = '/mnt/d/code/python/webo_write/flask/weibo-writer-agent/server/web/apps/weibo_UI/data/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 #
