ff09f21217fe13864e1d87d8743ef9c0ee678c18
Lucky2081 <luckyjasmine2081@gmail.com>
2024-08-03 21:59:51 +0800

    server: rag update.

--- server/rag/main.py
+++ server/rag/main.py
@@ -10,6 +10,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 # Local application/library specific imports.
 from configs import config
+from api import get_retrieve_inference, get_llm_inference
 
 app = FastAPI(
     title="RAG",
@@ -30,9 +31,14 @@ app.add_middleware(
 )
 
 
+class MessageItem(BaseModel):
+    role: str = Field(description="Choose from system, user, assistant...", default="user")
+    content: str = Field(default="prompt")
+
+
 class InfItem(BaseModel):
     # llm
-    messages: List[str] = Field(description="Please refer to openai to write")
+    messages: List[MessageItem] = Field(description="Please refer to openai to write")
     inference_service: str = Field(description="Choose from ollama, xinference, api...", default="ollama")
     model: str = Field(default="qwen2:1.5b-instruct-fp16")
     max_tokens: int = Field(default=4096)
@@ -41,7 +47,7 @@ class InfItem(BaseModel):
     timeout: int = Field(default=60)
     # rag
     rag: bool = Field(default=False)
-    kb_id: str = Field(default="0" * 32)
+    kb_id: str = Field(default="uuid" + "0" * 28)
     history_count: int = Field(default=10)
     top_k: int = Field(default=10)
     threshold_value: float = Field(default=0.0)
@@ -72,9 +78,28 @@ def inference(item: InfItem):
     logger.info('run inference')
     logger.info('item: {}'.format(item))
 
-    result = 'llm result'
+    kb_id = item.kb_id
+    messages = item.messages
+    messages = [{'role': message.role, 'content': message.content} for message in messages]
+    query = messages[-1]['content']
+    # query = '你好'
+    top_k = item.top_k
+    output_fields = ["text"]
+    retrieve_result = get_retrieve_inference(kb_id, query, top_k, output_fields)
+
+    logger.info('retrieve_result: {}'.format(retrieve_result))
+
+    inference_service = item.inference_service
+    model = item.model
+    max_tokens = item.max_tokens
+    stream = item.stream
+    temperature = item.temperature
+    timeout = item.timeout
+
+    llm_result = get_llm_inference(inference_service, messages, model, max_tokens, stream, temperature, timeout)
     data = {
-        'result': result,
+        'retrieve_result': retrieve_result,
+        'llm_result': llm_result,
     }
 
     return Response(success=True, code='000000', message='success', data=data)
